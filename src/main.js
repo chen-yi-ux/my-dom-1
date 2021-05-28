6d67b6adfbd2e75531575ba8d2fac4a0f066e12e
Cyer <1678447359@qq.com>
2021-05-28 18:34:55 +0800

    我的DOM编程第一版

---
+++ src/main.js
@@ -0,0 +1,58 @@
+//增
+const div = dom.create("<div>newDiv</div>");
+console.log(div);
+
+dom.after(test, div);
+
+const div3 = dom.create('<div id="parent"></div>');
+dom.wrap(test, div3);
+
+//删
+const div4 = dom.remove(remove);
+console.log(div4);
+
+const nodes = dom.empty(window.empty);
+console.log(nodes);
+
+//改
+dom.attr(test, "title", "Hi, I am Frank");
+const title = dom.attr(test, "title");
+console.log(title);
+
+dom.text(test, "你好，这是新的内容");
+
+console.log(`html: ${dom.html(test)}`);
+
+dom.style(test, { border: "1px solid red", color: "blue" });
+console.log(dom.style(test, "border"));
+dom.style(test, "border", "1px solid black");
+
+dom.class.add(test, "red");
+dom.class.add(test, "blue");
+dom.class.remove(test, "red");
+console.log(dom.class.has(test, "blue"));
+
+const fn = () => {
+  console.log("点击了");
+};
+dom.on(test, "click", fn);
+dom.off(test, "click", fn);
+
+//查
+const testDiv = dom.find("#test")[0];
+console.log(testDiv);
+const test2 = dom.find("#test2")[0];
+console.log(dom.find(".red", test2)[0]);
+
+console.log(dom.parent(test));
+
+console.log(dom.siblings(dom.find("#s2")[0]));
+
+console.log(dom.next(dom.find("#s2")[0]));
+
+console.log(dom.previous(dom.find("#s2")[0]));
+
+const t = dom.find("#travel")[0];
+dom.each(dom.children(t), (n) => dom.style(n, "color", "red"));
+
+console.log(dom.index(s2));
